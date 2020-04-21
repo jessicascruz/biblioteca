7903fa8b22bc96d097cf333fa1733e817d439005
Jéssica Cruz <jessica.santos648@gmail.com>
2020-04-21 00:03:51 -0300

    Validação de usuário para efetuar login

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -3,31 +3,18 @@ const connection = require('../database/connection');
 module.exports = {
     async create (require, response) {
         const { email, senha } = require.body;
-        
-        const usuario = await connection('usuarios')
-          .where({
-              'email': email,
-              'senha': senha
-          })
-          .select('nome', 'email')
-          .first()
-        
-        try {
-            console.log('senha', senha);
-            // if(senha) {
-
-            // }
-
+                
+            const usuario = await connection('usuarios')
+              .select('nome', 'email')
+              .where('senha', senha)
+              .where('email', email)
+              .first()
+            
             if(!usuario) {
                 return response.status(400).json({ error: 'Usuário não encontrado' });
             }
-            console.log(usuario)
-            
-            } catch(err) {
-                console.log(err);
-            }
-            
-                
-               await response.json(usuario);
+
+            return response.status(201).json({ message: 'Login efetuado com sucesso' });
+
     }
 }
